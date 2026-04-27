d1bf88c6fd460e0d080edbaa7e9f267669a92bcb
Melky Krisnando <pratamaadityudha@gmail.com>
2026-04-27 13:13:11 +0700

    Laravel & laragon

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\PeriodeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('Fakultas', FakultasController::class);
+Route::resource('/Fakultas', FakultasController::class);
+
+Route::resource('/Periode', PeriodeController::class);
